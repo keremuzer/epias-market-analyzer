dcbdf8702da9c553c4a095cf91da57ef2ed22035
Kerem Üzer <keremuzer33@gmail.com>
2025-10-26 19:38:14 +0300

    add ContractInfo model class

---
+++ Models/ContractInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EpiasMarketAnalyzer.Models
+{
+    public class ContractInfo
+    {
+        public DateTime ContractDate { get; set; }
+        public decimal TotalTransactionAmount { get; set; }
+        public decimal TotalTransactionQuantity { get; set; }
+        public decimal WeightedAveragePrice
+        {
+            get
+            {
+                if (TotalTransactionQuantity == 0)
+                    return 0;
+
+                return TotalTransactionAmount / TotalTransactionQuantity;
+            }
+        }
+    }
+}
